6ac79783ac516596ea99167b18e2bba176466359
Lars Gächter <lars.gaechter1998@gmail.com>
2022-02-22 21:04:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/js/sw.js')
+  navigator.serviceWorker.register('./js/sw.js', {scope: './js/'})
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err));
 }
