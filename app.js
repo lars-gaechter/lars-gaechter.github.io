d8c12287a2b538f625a9ffc49fe68d7bb13b5260
Lars Gächter <lars@escola.ch>
2022-02-23 16:17:37 +0100

    Update js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ if('serviceWorker' in navigator) {
     //.catch((err) => console.log('service worker not registered', err));
 }
 
-
+/*
 if (window.location.protocol === 'http:') {
   const requireHTTPS = document.getElementById('requireHTTPS');
   const link = requireHTTPS.querySelector('a');
   link.href = window.location.href.replace('http://', 'https://');
   requireHTTPS.classList.remove('hidden');
-}
+}*/
